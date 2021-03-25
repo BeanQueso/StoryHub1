07d4c45ed82ce7c270952c0547158d7fc0f92e1f
eshaant7 <68991885+eshaant7@users.noreply.github.com>
2021-03-25 22:41:20 +0200

    Add files via upload

---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native'
+import AppHeader from '../components/AppHeader'
+export default class WriteStoryScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <AppHeader/>
+
+                <TextInput
+                style = {styles.inputBox}
+                placeholder = "Story Title"
+                />
+
+                <TextInput
+                style = {styles.inputBox}
+                placeholder = "Author"
+                />
+
+                <TextInput
+                style = {styles.inputBox2}
+                multiline = {true}
+                placeholder = "Write Story Here"
+                />
+
+                <TouchableOpacity
+                style = {styles.button}
+                >
+                    <Text style = {styles.buttonText}>
+                    Submit
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    inputBox: {
+        marginTop: 10,
+        width: '80%',
+        alignSelf: 'center',
+        height: 40,
+        textAlign: 'center',
+        borderWidth: 4,
+        outline: 'none',
+      },
+      inputBox2: {
+        marginTop: 10,
+        width: '80%',
+        alignSelf: 'center',
+        height: 320,
+        textAlign: 'center',
+        borderWidth: 4,
+        outline: 'none',
+      },
+      button: {
+        marginLeft: 50,
+        marginTop: 25,
+        width: 120,
+        height: 50,
+        backgroundColor: '#87ceeb',
+        borderRadius: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+      },
+   
+      buttonText: {
+        fontFamily: 'Arial',
+        fontSize: 14,
+        textAlign: 'center',
+        color:'navy'
+      },
+})
